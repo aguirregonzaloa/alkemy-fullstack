654d2bdd5bd20b8d8a8a9a6f978dbeba140e3cb4
Darkzalodev <gonza1810@gmail.com>
2022-09-30 15:29:23 -0300

    Creating connection from client to server packages using proxy and axios

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:4040',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/server/, ''),
+        ws: true,
+      },
+    },
+  },
+  hmr: {
+    host: 'http://localhost:4040',
+  },
+});
