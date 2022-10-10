f2efacbf39bed806bbff7fd143e487bcb0399ba0
Darkzalodev <gonza1810@gmail.com>
2022-10-10 20:30:27 -0300

    Changing some styles

--- packages/client/src/pages/Login/Login.jsx
+++ packages/client/src/pages/Login/Login.jsx
@@ -2,18 +2,39 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>
-      <h2>Login:</h2>
-      <form action="">
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" required />
+    <div className="m-auto w-full pt-4">
+      <h2 className="font-bold text-center mb-4">Login:</h2>
+      <form action="" className="w-[550px] m-auto ">
+        <div className="flex justify-between mb-4">
+          <label htmlFor="email" className="w-[25%]">
+            Email:
+          </label>
+          <input
+            className="w-[75%] border border-black border-solid rounded"
+            type="email"
+            name="email"
+            id="email"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" required />
+        <div className="flex justify-between mb-4">
+          <label className="w-[25%]" htmlFor="password">
+            Password:
+          </label>
+          <input
+            className="w-[75%] border border-black border-solid rounded"
+            type="password"
+            name="password"
+            id="password"
+            required
+          />
         </div>
-        <button type="submit">login</button>
+        <button
+          className="bg-slate-400 text-white font-bold rounded p-3 w-full"
+          type="submit"
+        >
+          login
+        </button>
       </form>
     </div>
   );
--- packages/client/src/pages/Register/Register.jsx
+++ packages/client/src/pages/Register/Register.jsx
@@ -2,22 +2,59 @@ import React from 'react';
 
 const Register = () => {
   return (
-    <div>
-      <h2>Register: </h2>
-      <form action="">
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" name="username" required />
+    <div className="m-auto w-full pt-4">
+      <h2 className="text-center font-bold mb-4">Register: </h2>
+      <form action="" className="w-[650px] m-auto">
+        <div className="flex justify-between mb-4">
+          <label htmlFor="username" className="w-[30%]">
+            Username:
+          </label>
+          <input
+            className="border border-black border-solid rounded w-[70%]"
+            type="text"
+            name="username"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="text" name="email" required />
+        <div className="flex justify-between mb-4">
+          <label htmlFor="email" className="w-[30%]">
+            Email:
+          </label>
+          <input
+            className="border border-black border-solid rounded w-[70%]"
+            type="text"
+            name="email"
+            required
+          />
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password" required />
+        <div className="flex justify-between mb-4">
+          <label htmlFor="password" className="w-[30%]">
+            Password:
+          </label>
+          <input
+            className="border border-black border-solid rounded w-[70%]"
+            type="password"
+            name="password"
+            required
+          />
         </div>
-        <button type="submit">register</button>
+        <div className="flex justify-between mb-4">
+          <label htmlFor="password" className="w-[30%]">
+            Confirmed Password:
+          </label>
+          <input
+            className="border border-black border-solid rounded w-[70%]"
+            type="password"
+            name="password"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className=" bg-slate-500 text-neutral-50 font-bold rounded p-3 w-full"
+        >
+          register
+        </button>
       </form>
     </div>
   );
