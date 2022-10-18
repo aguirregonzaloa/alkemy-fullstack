a8993b846b9383bed4a71b22df7e1aaae417ad70
Darkzalodev <gonza1810@gmail.com>
2022-10-18 16:18:34 -0300

    Adding Category page functionality of creating a new category

--- packages/client/src/pages/Category/Category.jsx
+++ packages/client/src/pages/Category/Category.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import * as React from 'react';
+import { getallCategories, postCategory } from '../../shared/api/categories';
 
 const Category = () => {
+  const [categories, setCategories] = React.useState([]);
+  const [category, setCategory] = React.useState({ name: '' });
+  const [createCategory, setCreateCategory] = React.useState({
+    data: null,
+    error: null,
+    loading: false,
+  });
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState('');
+
+  async function getCategories() {
+    const { data, error, loading } = await getallCategories();
+    setCategories(data);
+    setError(error);
+    setLoading(loading);
+  }
+
+  React.useEffect(() => {
+    setLoading(true);
+    getCategories();
+  }, []);
+
+  const handleChange = (e) => {
+    const name = e.target.value;
+    setCategory({ ...category, name });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setCreateCategory({ ...createCategory, loading: true });
+    const token = localStorage.getItem('Token');
+    const { data, error, loading } = await postCategory(category, token);
+    setCreateCategory({ ...createCategory, data, error, loading });
+    getCategories();
+  };
   return (
     <div className="pt-4">
       <h2 className="text-center font-bold mb-10"> Category: </h2>
-      <form action="" className="w-[550px] m-auto">
+      <form onSubmit={handleSubmit} className="w-[550px] m-auto">
         <div className="flex items-center gap-2 mb-4">
           <label htmlFor="name">Name: </label>
           <input
@@ -12,13 +48,39 @@ const Category = () => {
             type="text"
             name="name"
             id="name"
+            value={category.name}
+            onChange={handleChange}
             required
           />
         </div>
-        <button className="bg-slate-400 text-white font-bold rounded p-3 w-full">
+        {createCategory.data && (
+          <p className="text-green-800 bg-green-300 font-bold">
+            Created a new Category successfully
+          </p>
+        )}
+        {createCategory.error && (
+          <p className="text-red-800 bg-red-300 font-bold">
+            Category was not created
+          </p>
+        )}
+        <button
+          className={
+            createCategory.loading
+              ? 'bg-slate-900 text-white font-bold rounded p-3 w-full'
+              : 'bg-slate-400 text-white font-bold rounded p-3 w-full'
+          }
+        >
           New Category
+          {createCategory.loading ? <span>...</span> : ''}
         </button>
       </form>
+      <div className="w-[550px] m-auto mt-10">
+        <h3>List of Categories:</h3>
+        {categories &&
+          categories.map((item) => {
+            return <p key={item.id}>{item.name}</p>;
+          })}
+      </div>
     </div>
   );
 };
--- packages/client/src/shared/api/categories.js
+++ packages/client/src/shared/api/categories.js
@@ -8,10 +8,27 @@ export const getallCategories = async () => {
   try {
     const res = await api.get(baseURL);
     data = await res.data.categories;
-    loading = false;
-    return { data, error, loading };
   } catch (err) {
     error = err.response.data.error;
+  } finally {
+    loading = false;
+    return { data, error, loading };
+  }
+};
+
+export const postCategory = async (category, token) => {
+  let data, error;
+  let loading = true;
+  try {
+    const res = await api.post(baseURL, category, {
+      headers: {
+        'x-access-token': `${token}`,
+      },
+    });
+    data = await res.data.category;
+  } catch (err) {
+    error = err.response.data;
+  } finally {
     loading = false;
     return { data, error, loading };
   }
